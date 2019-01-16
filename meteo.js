8b0e71c44f815d84b1cd5698b07a5e64083fd0e4
batcrash <baptiste.lamblot@gmail.com>
2019-01-16 09:17:25 +0100

    projet

--- meteo.js
+++ meteo.js
@@ -4,18 +4,11 @@ var xhr = new XMLHttpRequest();
 // http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?
 // date=1527811200&opacity=0.9&fill_bound=true&appid={api_key}
 
-<<<<<<< HEAD
 var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var units = "metric";
-=======
-var base_url = "http://api.openweathermap.org/data/2.5/forecast";
->>>>>>> fe07c7feb615038d3d87961fc6059da076cf000e
 var city = "Metz";
 var appid = "f5e810531af1756846022c6f387acf25";
-<<<<<<< HEAD
-=======
 var units = "metric";
->>>>>>> 880859cb0052986452c7d1fa9896c76e83d713ee
 
 function get_url() {
     return base_url + "?"
@@ -28,47 +21,11 @@ function init_page() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("url").innerHTML = get_url();
-<<<<<<< HEAD
+
             var response = JSON.parse(this.responseText);
             var temperature = response.main.temp;
+
             var icon = response.weather[0].icon;
-            var src = "http://openweathermap.org/img/w/" + icon + ".png";
-            document.getElementById("meteo").innerHTML = temperature;
-            document.getElementById("icon").src = src;
-        }
-    };
-    
-    xhr.open("GET", get_url(), true)
-    xhr.send()
-}
-
-function get_temperature() {
-    city = document.getElementById("ville").value;
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("url").innerHTML = get_url();
-            
-            if(document.getElementById("url_visibility").checked) {
-                document.getElementById("url").style.display = "block";
-            }
-            else {
-                document.getElementById("url").style.display = "none";
-            }
-            var response = JSON.parse(this.responseText);
-            var temperature = response.main.temp;
-            var icon = response.weather[0].icon;
-            var src = "http://openweathermap.org/img/w/" + icon + ".png";
-            document.getElementById("icon").src = src;
-            document.getElementById("meteo").innerHTML = temperature;
-        }
-    };
-    
-=======
-
-            var response = JSON.parse(this.responseText);
-            var temperature = response.list[0].main.temp;
-
-            var icon = response.list[0].weather[0].icon;
 
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
 
@@ -95,15 +52,14 @@ function get_temperature() {
             }
 
             var response = JSON.parse(this.responseText);
-            var temperature = response.list[0].main.temp;
-            var icon = response.list[0].weather[0].icon;
+            var temperature = response.main.temp;
+            var icon = response.weather[0].icon;
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon").src = src;
             document.getElementById("meteo").innerHTML = temperature;
         }
     };
     
->>>>>>> 880859cb0052986452c7d1fa9896c76e83d713ee
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
