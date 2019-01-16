5ff88064b30f0b9af62fd5f06ef7eeb2ab39e83b
MaximeKlekowicki <maxime.klekowicki@gmail.com>
2019-01-16 08:39:33 +0100

    Message

--- meteo.js
+++ meteo.js
@@ -6,21 +6,67 @@ var xhr = new XMLHttpRequest();
 
 var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var city = "Metz";
-var appid = "3c084bd74c2f77f02d6d6c30c2018bf0";
+var units = "metric";
+var appid = "f5e810531af1756846022c6f387acf25";
 
-function get_url() {
+
+function get_url() 
+{
     return base_url + "?"
         + "q=" + city + "&"
+        + "units=" + units +"&"
         + "appid=" + appid;
 }
 
-function init_page() {
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("meteo").innerHTML = this.responseText;
+function init_page() 
+{
+    xhr.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            document.getElementById("url").innerHTML = get_url();
+            var response = JSON.parse(this.responseText);
+            var temperature = response.main.temp;
+            var icon = response.weather[0].icon;
+            var src = "https://openweathermap.org/img/w/" + icon + ".png";
+            document.getElementById("meteo").innerHTML = temperature;
+            document.getElementById("icon").src = src;
         }
-    };
+    }
     
-    xhr.open("GET", get_url(), true)
-    xhr.send()
+    xhr.open("GET", get_url(), true);
+    xhr.send();
+}
+
+function get_temperature() 
+{
+    city = document.getElementById("ville").value;
+    xhr.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            document.getElementById("url").innerHTML = get_url();
+            var response = JSON.parse(this.responseText);
+            var temperature = response.main.temp;
+            var icon = response.weather[0].icon;
+            var src = "https://openweathermap.org/img/w/" + icon + ".png";
+            document.getElementById("meteo").innerHTML = temperature;
+            document.getElementById("icon").src = src;
+
+            if(document.getElementById("url_visibility").checked)
+            {
+                document.getElementById("url").style.display ="block";
+            }
+            else
+            {
+                document.getElementById("url").style.display = "none";
+            }
+            var response = JSON.parse(this.responseText);
+            var temperature = response.main.temp;
+            document.getElementById("meteo").innerHTML = temperature;
+        }
+    }
+    
+    xhr.open("GET", get_url(), true);
+    xhr.send();
 }
