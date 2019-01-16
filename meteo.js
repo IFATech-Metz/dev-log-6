2d41d0d3ab4eba0eadff44a5018f3494367c3a87
quenfil <quentin.fillou@stagiairesifa.fr>
2019-01-16 10:38:35 +0100

    Prévision à 5 jours - Étape 4

--- meteo.js
+++ meteo.js
@@ -22,38 +22,38 @@ function init_page() {
             document.getElementById("url").innerHTML = get_url();
 
             var response = JSON.parse(this.responseText);
-            var temperature = response.list[0].main.temp;
+            var temperature = Math.round(response.list[0].main.temp);
 
             var icon = response.list[0].weather[0].icon;
 
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
 
             document.getElementById("meteo").innerHTML = temperature;
-            temperature = response.list[1].main.temp;
+            temperature = Math.round(response.list[1].main.temp);
 
             document.getElementById("icon").src = src;
             icon = response.list[1].weather[0].icon;
 
             document.getElementById("meteo2").innerHTML = temperature;
-            temperature = response.list[2].main.temp;
+            temperature = Math.round(response.list[2].main.temp);
 
             document.getElementById("icon2").src = src;
             icon = response.list[2].weather[0].icon;
 
             document.getElementById("meteo3").innerHTML = temperature;
-            temperature = response.list[3].main.temp;
+            temperature = Math.round(response.list[3].main.temp);
 
             document.getElementById("icon3").src = src;
             icon = response.list[3].weather[0].icon;
 
             document.getElementById("meteo4").innerHTML = temperature;
-            temperature = response.list[4].main.temp;
+            temperature = Math.round(response.list[4].main.temp);
 
             document.getElementById("icon4").src = src;
             icon = response.list[4].weather[0].icon;
 
             document.getElementById("meteo5").innerHTML = temperature;
-            temperature = response.list[5].main.temp;
+            temperature = Math.round(response.list[5].main.temp);
 
             document.getElementById("icon5").src = src;
             icon = response.list[5].weather[0].icon;
@@ -79,31 +79,31 @@ function get_temperature() {
             }
 
             var response = JSON.parse(this.responseText);
-            var temperature = response.list[0].main.temp;
+            var temperature = Math.round(response.list[0].main.temp);
             var icon = response.list[0].weather[0].icon;
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon").src = src;
             document.getElementById("meteo").innerHTML = temperature;
 
-            temperature = response.list[1].main.temp;
+            temperature = Math.round(response.list[1].main.temp);
             icon = response.list[1].weather[0].icon;
             src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon2").src = src;
             document.getElementById("meteo2").innerHTML = temperature;
 
-            temperature = response.list[2].main.temp;
+            temperature = Math.round(response.list[2].main.temp);
             icon = response.list[2].weather[0].icon;
             src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon3").src = src;
             document.getElementById("meteo3").innerHTML = temperature;
 
-            temperature = response.list[3].main.temp;
+            temperature = Math.round(response.list[3].main.temp);
             icon = response.list[3].weather[0].icon;
             src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon4").src = src;
             document.getElementById("meteo4").innerHTML = temperature;
 
-            temperature = response.list[4].main.temp;
+            temperature = Math.round(response.list[4].main.temp);
             icon = response.list[4].weather[0].icon;
             src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon5").src = src;
