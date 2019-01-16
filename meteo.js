42e9881cbdffb5459af723bf38f9b0e0bc40979b
batcrash <baptiste.lamblot@gmail.com>
2019-01-16 10:44:09 +0100

    projet

--- meteo.js
+++ meteo.js
@@ -7,8 +7,10 @@ var xhr = new XMLHttpRequest();
 var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var units = "metric";
 var city = "Metz";
-var appid = "f5e810531af1756846022c6f387acf25";
+var appid = "ff7df6d0fadc12d3893f4cab80f6aee5";
 var units = "metric";
+var response = JSON.parse(this.responseText);
+var temperature = response.main.temp;
 
 function get_url() {
     return base_url + "?"
@@ -22,42 +24,8 @@ function init_page() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("url").innerHTML = get_url();
 
-            var response = JSON.parse(this.responseText);
-            var temperature = response.main.temp;
-
             var icon = response.weather[0].icon;
-
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
-
-            document.getElementById("meteo").innerHTML = temperature;
-            temperature = response.list[1].main.temp;
-
-            document.getElementById("icon").src = src;
-            icon = response.list[1].weather[0].icon;
-
-            document.getElementById("meteo2").innerHTML = temperature;
-            temperature = response.list[2].main.temp;
-
-            document.getElementById("icon2").src = src;
-            icon = response.list[2].weather[0].icon;
-
-            document.getElementById("meteo3").innerHTML = temperature;
-            temperature = response.list[3].main.temp;
-
-            document.getElementById("icon3").src = src;
-            icon = response.list[3].weather[0].icon;
-
-            document.getElementById("meteo4").innerHTML = temperature;
-            temperature = response.list[4].main.temp;
-
-            document.getElementById("icon4").src = src;
-            icon = response.list[4].weather[0].icon;
-
-            document.getElementById("meteo5").innerHTML = temperature;
-            temperature = response.list[5].main.temp;
-
-            document.getElementById("icon5").src = src;
-            icon = response.list[5].weather[0].icon;
 
         }
     };
@@ -79,15 +47,15 @@ function get_temperature() {
                 document.getElementById("url").style.display = "none";
             }
 
-            var response = JSON.parse(this.responseText);
-            var temperature = response.main.temp;
             var icon = response.weather[0].icon;
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
             document.getElementById("icon").src = src;
             document.getElementById("meteo").innerHTML = temperature;
         }
     };
-    
+
+
+
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
