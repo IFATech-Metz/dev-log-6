7ec2d23c33dfd3e3ba353965576dfea8349890ad
quenfil <quentin.fillou@stagiairesifa.fr>
2019-01-18 16:21:37 +0100

    Réécriture du code par Michael Récupération des données avec gestion asynchrone Il reste à déterminer leur affichage

--- meteo.js
+++ meteo.js
@@ -9,6 +9,8 @@ var city = "Metz";
 var appid = "ff7df6d0fadc12d3893f4cab80f6aee5";
 var units = "metric";
 
+let forcastArray = [];
+
 function get_url() {
     return base_url + "?"
         + "q=" + city + "&"
@@ -16,7 +18,7 @@ function get_url() {
         + "appid=" + appid;
 }
 
-
+/*
 function init_page() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -41,20 +43,67 @@ function init_page() {
                 }
             }
         }
-    };
+    }
     
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
+*/
+
 
 function handleKeyPress(e)
 {
     var key = e.key;
-    if(key === "Enter") get_temperature();
+    if(key == "Enter"){
+        /*get_temperature();
+        get_pressure();*/
+        requete(ville);
+    }
+}  
+
+
+// handle key -> xhr
+// xhr ->  objet - > get temp
+// get temp -> get pression
+
+function requete(ville){
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            parsingData(this.responseText);
+        }
+        else parsingData(false);
+    }
+    xhr.open("GET", get_url(), true);
+    xhr.send();
 }
 
+function parsingData(dataJ){
+    let parsedJ = (dataJ ? JSON.parse(dataJ) : false);
+    if(parsedJ){
+        for(let i = 0; i < 5; i++){
+            let objetData = {
+                "pression": "",
+                "temperature": "",
+                "humidite": "",
+                "windspeed": "", 
+            };            
+            let n = i*8;
+            objetData.pression = parsedJ.list[n].main.pressure;
+            objetData.temperature = parsedJ.list[n].main.temp;
+            objetData.humidite = parsedJ.list[n].main.humidity;
+            objetData.windspeed = parsedJ.list[n].wind.speed;
+            forcastArray = [...forcastArray, objetData]
+        }
+    }
+    //console.log(forcastArray);
+}
 
-function get_temperature() {
+function getData(ville){
+    requete(ville);
+}
+
+/*function get_temperature() {
+    console.log("hello");
     city = document.getElementById("ville").value;
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -85,8 +134,44 @@ function get_temperature() {
                 }
             }
         }
-    };
+    }
     
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
+*/
+
+/*function get_pressure() {
+    city = document.getElementById("ville").value;
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("url").innerHTML = get_url();
+            
+            /*if(document.getElementById("url_visibility").checked){
+                document.getElementById("url").style.display = "block";
+            }
+            else{
+                document.getElementById("url").style.display = "none";
+            }*/
+            //if (document.getElementById("pression").checked){
+                //var response = JSON.parse(this.responseText);
+                //var pressure;
+                //var pas;
+                //for (pas = 1;pas < 6; pas++){
+                    //pressure=response.list[pas-1].main.pressure;
+                    //document.getElementById("pressure" + pas).innerHTML = pressure;
+                    //document.getElementById("pressure" + pas).style.display = block;
+                //}
+            //}
+            /*else{
+                for (pas = 1;pas < 6; pas++){
+                    document.getElementById("pressure" + pas).style.display = "none";
+                }
+            }*/
+            
+        //}
+    //}
+
+    //xhr.open("GET", get_url(), true);
+    //xhr.send();
+//}
